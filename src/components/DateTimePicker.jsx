e53cdb0cd8f29d037d06269918645eeb1b7d4352
Calixte <tchicoulo@hotmail.fr>
2022-12-09 02:30:56 +0100

    added datepicker library

--- src/components/DateTimePicker.jsx
+++ src/components/DateTimePicker.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 const DateTimePicker = () => {
   const [startDate, setStartDate] = useState(new Date());
