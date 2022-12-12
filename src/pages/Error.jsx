3b2dab0b8b7dd20296f79d6373c2dafc72e4be8b
Calixte <tchicoulo@hotmail.fr>
2022-12-12 14:57:13 +0100

    added style to error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Error = () => {
   return (
     <div className="error">
-      <h1>Error Page</h1>
+      <h1>Error 404</h1>
+      <h2>Page not found</h2>
+      <NavLink className={"navlink"} to="/">
+        Home
+      </NavLink>
     </div>
   );
 };
