8e830f26e825abb9b015cde73469f27b7e3dcfac
Calixte <tchicoulo@hotmail.fr>
2022-12-13 17:19:31 +0100

    Added LabInputOption Component with Custom different Data

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,12 +2,26 @@ import React, { useRef } from "react";
 import DateTimePicker from "./DateTimePicker";
 import LabInput from "./LabInput";
 import moment from "moment";
+import states from "../services/dataStates";
+import LabInputOption from "./LabInputOption";
 
 const Form = () => {
   const firstNameInput = useRef();
   const lastNameInput = useRef();
   const dateOfBirth = useRef();
   const startDate = useRef();
+  const street = useRef();
+  const city = useRef();
+  const state = useRef();
+  const department = useRef();
+
+  const departments = [
+    "Sales",
+    "Marketing",
+    "Engineering",
+    "Human Ressources",
+    "Legal",
+  ];
 
   const saveEmployee = (e) => {
     e.preventDefault();
@@ -17,6 +31,10 @@ const Form = () => {
       lastName: lastNameInput.current.value,
       dateOfBirth: moment(dateOfBirth.current.props.selected).format("L"),
       startDate: moment(startDate.current.props.selected).format("L"),
+      street: street.current.value,
+      city: city.current.value,
+      state: state.current.value,
+      department: department.current.value,
     };
 
     console.log(data);
@@ -33,6 +51,17 @@ const Form = () => {
       <LabInput label="Last Name" ref={lastNameInput} />
       <DateTimePicker label="Date of Birth" ref={dateOfBirth} />
       <DateTimePicker label="Start Date" ref={startDate} />
+      <div className="adress-area">
+        <span>Address</span>
+        <LabInput label="Street" ref={street} />
+        <LabInput label="City" ref={city} />
+        <LabInputOption data={states} label="States" ref={state} />
+        <LabInputOption
+          data={departments}
+          label="Department"
+          ref={department}
+        />
+      </div>
 
       <input type="submit" className="submit-employee" value="Save" />
     </form>
---
+++ src/components/LabInputOption.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+// import { useState } from "react";
+
+const LabInputOption = React.forwardRef((props, ref) => {
+  // const [option, setOption] = useState("");
+  return (
+    <div className="lab-input-option">
+      <label>{props.label}</label>
+      <select className="select" ref={ref}>
+        {Array.from(props.data).map((element) => (
+          <option
+            value={element.abbreviation ? element.abbreviation : element}
+            key={element.name ? element.name : element}
+            onChange={(e) => e.target.value}
+          >
+            {element.name ? element.name : element}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
+});
+
+export default LabInputOption;
